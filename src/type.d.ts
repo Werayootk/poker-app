e0b5a89c2c218666dfe269098a1cd95922feb365
Werayootk <werayoot5800@gmail.com>
2021-09-06 06:28:38 +0700

    Add project

---
+++ src/type.d.ts
@@ -0,0 +1,13 @@
+interface ITodo {
+    id: number;
+    title: string;
+    description: string;
+    status: boolean;
+  }
+  
+  type ContextType = {
+    todos: ITodo[];
+    saveTodo: (todo: ITodo) => void;
+    updateTodo: (id: number) => void;
+  };
+  
